afbe75db265674c3857e33657d3a18f07a98b8b2
barivan72 <ivanbar72@yahoo.com>
2026-09-05 18:11:26 +0100

    Cache multilingual interface assets

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE="bvs-protocol-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
+const CACHE="bvs-protocol-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./i18n.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(fetch(e.request).then(r=>{
